a09096add5fd4d2908b22f33b92230d9555db290
pcoloigner <philippe.coloigner@free.fr>
2019-09-24 03:51:10 +0200

    import Lucid

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,14 @@ module Main where
 
 import Web.Spock
 import Web.Spock.Config
+import Web.Spock.Lucid (lucid)
+import Lucid 
 
 
 app :: SpockM () () () ()
-app = get root (text "Hello!")
+app = get root $ lucid $ do
+    h1_ "Hello"
+    p_ "How are you today"
 
 main :: IO ()
 main = do
