7a96e911113daa67ee7fc22532be98b6dbc47ea6
pcoloigner <philippe.coloigner@free.fr>
2019-09-25 05:32:37 +0200

    Test première application avec Scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
-
-import Web.Spock
-import Web.Spock.Config
-import Web.Spock.Lucid (lucid)
-import Lucid 
-
-
-app :: SpockM () () () ()
-app = get root $ lucid $ do
-    h1_ "Hello"
-    p_ "How are you today"
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Foldable (for_)
+import qualified Data.Text as Text (pack)
+import           System.Environment (getEnv, getEnvironment)
+import           Snap.Core
+                    ( Snap
+                    , ifTop
+                    , modifyResponse
+                    , setHeader
+                    , writeText
+                    )
+import           Snap.Http.Server (httpServe, setPort)
 
 main :: IO ()
 main = do
-    cfg <- defaultSpockCfg () PCNoDatabase ()
-    runSpock 8080 (spock cfg app)
+    port <- read <$> getEnv "PORT"
+    let config = setPort port mempty
+    httpServe config site
+
+site :: Snap ()
+site = ifTop indexHandler
+
+indexHandler :: Snap ()
+indexHandler = do
+    modifyResponse $ setHeader "Content-Type" "text/html"
+    writeText "<p>Hello from Haskell! and Philippe !</p>"
+    env <- liftIO getEnvironment
+    writeText "<ul>"
+    for_ env $ \(k, v) ->
+        writeText $ Text.pack ("<li> yeah ! " ++ k ++ "=" ++ v ++ "</li>")
+    writeText "</ul>"
